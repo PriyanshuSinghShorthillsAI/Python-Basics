abc0cc584a5051462487a39aa221dae1602b61dd
priyanshu <priyanshu@shorthills.ai>
2024-10-08 12:17:51 +0530

    Learned about variables and its Scope

---
+++ Variables_and_its_scope.py
@@ -0,0 +1,10 @@
+def my_function():
+    x = 10  
+    print(x)
+
+my_function() 
+print(x)  
+
+"""
+Note 1: Here x is defined inside my_function() so as soon as function scope ends the value of x get over . So in Line no 6 it will get error.
+"""
