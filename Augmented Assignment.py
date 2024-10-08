f22787f44785fb45cb29d9fca6c360ce608183a0
priyanshu <priyanshu@shorthills.ai>
2024-10-08 12:35:22 +0530

    Learned about Augmented Assignment

---
+++ Augmented Assignment.py
@@ -0,0 +1,41 @@
+
+x = 10
+
+# Augmented Assignment Operators
+x += 5  
+print(x)  
+
+x -= 3  
+print(x)  
+
+x *= 2  
+print(x) 
+
+x /= 4 
+print(x)  
+
+x //= 2  
+print(x)  
+
+x %= 2  
+print(x)  
+
+x **= 3  
+print(x)  
+
+
+y = 4
+y &= 2  
+print(y)  
+
+y |= 5  
+print(y)  
+
+y ^= 3  
+print(y) 
+
+y <<= 1  
+print(y) 
+
+y >>= 2  
+print(y)  
