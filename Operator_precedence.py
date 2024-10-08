974b8f14fef60b4d213c5d73983cfac8957506c5
priyanshu <priyanshu@shorthills.ai>
2024-10-08 11:59:49 +0530

    Learned about Operator Precedence

---
+++ Operator_precedence.py
@@ -0,0 +1,26 @@
+
+x = 10 + 4 * 3 ** 2 / 2
+
+print(x)
+
+x = (10 + 4) * (3 ** 2) / 2
+print(x) 
+
+
+"""
+** (Exponentiation)
++x, -x, ~x (Unary plus, Unary minus, Bitwise NOT)
+*, /, //, % (Multiplication, Division, Floor Division, Modulus)
++, - (Addition, Subtraction)
+<<, >> (Bitwise Shift Operators)
+& (Bitwise AND)
+^ (Bitwise XOR)
+| (Bitwise OR)
+Comparisons (==, !=, >, <, >=, <=)
+not (Logical NOT)
+and (Logical AND)
+or (Logical OR)
+
+Note: We Can Overright the Precedence by using bracket.
+
+"""
