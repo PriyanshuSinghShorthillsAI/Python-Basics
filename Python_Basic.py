f8a6f53e78643c0f9c970e2a661418f44a221375
priyanshu <priyanshu@shorthills.ai>
2024-10-08 11:37:20 +0530

    See the basic datatype

---
+++ Python_Basic.py
@@ -0,0 +1,25 @@
+a = 45;
+name = "Priyanshu"
+b = 45.98
+chr = 'c';
+
+print(a);
+print(name);
+print(b);
+print(chr);
+print(type(a));
+print(type(name));
+print(type(b));
+print(type(chr));
+a=97;
+print(a);
+#name[0]="k";
+name = "Papu";
+print(name);
+
+"""
+Note 1 : Here we don't need to explicitly named the type of datatype.
+Note 2 : we can change the datatype of int,char,float but we cannot change the character value of any string
+Note 3: But We can Reasign the value of a string.
+
+"""
