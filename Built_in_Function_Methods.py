6b9f25ba3a3445ccf2feb8ff350d118ce8d3cdcd
priyanshu <priyanshu@shorthills.ai>
2024-10-08 15:31:19 +0530

    Built-in-function

---
+++ Built_in_Function_Methods.py
@@ -0,0 +1,36 @@
+# Built in Function and Methods
+
+print("Hello, World!")  
+
+name = "Priyanshu"
+print(len(name)) 
+
+num = 42
+print(type(num))  
+
+negative_num = -10
+print(abs(negative_num)) 
+
+numbers = [1, 2, 3, 4, 5]
+print(sum(numbers))  
+
+
+print(max(10, 20, 5))  
+
+print(min(10, 20, 5)) 
+
+numbers = [3, 1, 4, 2]
+print(sorted(numbers))  
+
+num = 3.14159
+print(round(num, 2))  
+
+
+user_input = input("Enter something: ")
+print(f"You entered: {user_input}")
+
+
+
+
+
+
