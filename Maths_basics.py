0865775ac764de3d99ad3eb5fb2ea78848543f70
priyanshu <priyanshu@shorthills.ai>
2024-10-08 11:48:56 +0530

    Maths Function hands on Practise

---
+++ Maths_basics.py
@@ -0,0 +1,13 @@
+import math
+
+print(abs(-10))           
+print(round(3.14159, 2))    
+
+print(math.exp(1))         
+print(math.log(100, 10))    
+
+
+print(math.sin(math.pi / 2))  
+
+print(pow(2, 3))           
+print(math.sqrt(25))       
