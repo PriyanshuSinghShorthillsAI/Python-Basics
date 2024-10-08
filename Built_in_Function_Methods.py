b947f73f31b3bc3a001a0e4fc8010d253e922375
priyanshu <priyanshu@shorthills.ai>
2024-10-08 15:40:26 +0530

    Built-in-methods

--- Built_in_Function_Methods.py
+++ Built_in_Function_Methods.py
@@ -31,6 +31,57 @@ print(f"You entered: {user_input}")
 
 
 
+text = "hello"
+print(text.upper())  
+
+text = "apple,banana,orange"
+fruits = text.split(",")
+print(fruits)  
+
+
+text = "I love Python"
+print(text.replace("love", "like"))  # Output: I like Python
+
+
+fruits = ["apple", "banana"]
+fruits.append("orange")
+print(fruits)  
+
+
+fruits = ["apple", "banana", "orange"]
+fruits.remove("banana")
+print(fruits)  
+
+
+numbers = [3, 1, 4, 2]
+numbers.sort()
+print(numbers)  
+
+
+fruits = ["apple", "banana", "orange"]
+fruit = fruits.pop()  # Removes 'orange'
+print(fruit)  # Output: orange
+print(fruits)  
+
+person = {"name": "Priyanshu", "age": 24}
+print(person.keys())  # dict_keys(['name', 'age'])
+
+print(person.values())  # dict_values(['Priyanshu', 24])
+
+
+
+fruits = ["apple", "banana", "orange"]
+
+print(len(fruits))  
+print(fruits[0].upper())  
+
+fruits.append("mango")  
+print(fruits)  
+
+
+
+
+
 
 
 
