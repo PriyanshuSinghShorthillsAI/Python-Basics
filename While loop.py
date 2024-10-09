602bc17d0a62524d6898647c601c0d001b6f4c1b
priyanshu <priyanshu@shorthills.ai>
2024-10-09 09:48:19 +0530

    Learned about while loop

---
+++ While loop.py
@@ -0,0 +1,14 @@
+counter = 0
+
+while counter < 5:
+    print(f"Counter is at: {counter}")
+    counter += 1  # Increment the counter
+
+total = 0
+num = 1
+
+while num <= 5:
+    total += num
+    num += 1
+
+print(f"Total sum from 1 to 5 is: {total}")
